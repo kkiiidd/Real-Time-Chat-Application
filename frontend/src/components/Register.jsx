29b1d9a89065b2cb295750d921cfaa39cebd84e3
kkiiidd <kofeine@outlook.com>
2023-02-02 18:05:01 +0800

    login register handler p1

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom';
 const Register = () => {
+    const [state,setState]=useState({
+        userName:'',
+        email:'',
+        password:'',
+        confirmPassword:'',
+        image:''
+    })
+    const myPromise = (e)=> new Promise((resolve,reject)=>{
+        setState({ 
+            ...state,
+            [e.target.name]:e.target.value
+        })
+        resolve('success');
+    })
+const inputHandler = (e)=>{
+
+    myPromise(e).then(res=>{
+        console.log(state)
+    })
+    // console.log(state);
+}
   return (
     <div className='account register'>
         <div className='card'>
@@ -10,28 +31,28 @@ const Register = () => {
             <div className="card-body">
                 <form className='form'>
                     <div className='form-item'>
-                        <label htmlFor="username">User Name</label>
-                        <input type="text" name="username" id="username" placeholder='User Name' /> 
+                        <label htmlFor="userName">User Name</label>
+                        <input type="text" name="userName" value={state.userName} onChange={inputHandler} id="userName" placeholder='User Name' /> 
                     </div>
 
                     <div className='form-item'>
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" placeholder='Email' />
+                        <input type="text" name="email" value={state.email} onChange={inputHandler} id="email" placeholder='Email' />
                     </div>
 
                     <div className='form-item'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" placeholder='Password' />
+                        <input type="password" name="password" value={state.password} onChange={inputHandler} id="password" placeholder='Password' />
                     </div>
 
                     <div className='form-item'>
                         <label htmlFor="confirmPassword">Confirm Password</label>
-                        <input type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm Password' />
+                        <input type="password" name="confirmPassword" value={state.confirmPassword} onChange={inputHandler} id="confirmPassword" placeholder='Confirm Password' />
                     </div>
 
                     <div className='form-item'>
                         <label htmlFor="image">Select Image</label>
-                        <input type="file" name="image" id="image" placeholder='Select Image' />
+                        <input type="file" name="image" value={state.image} onChange={inputHandler} id="image" placeholder='Select Image' />
                     </div>
 
                     <div className='form-item'>
